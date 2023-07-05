b503d66074ddcddc66fbaaa3852e08c316f27afa
daniel <danielp1239@gmail.com>
2023-07-05 17:54:08 +0300

    M

--- Pipeline-Docker-Compose/Flask-Project/manageVersion.py
+++ Pipeline-Docker-Compose/Flask-Project/manageVersion.py
@@ -11,7 +11,7 @@ if existing_versions:
     next_version = latest_version + 0.1
 else:
     next_version = 1.0
-
+    
 # Format the version number to one decimal place
 next_version = f"{next_version:.1f}"
 image_name = f"danielpinhas/flask-compose:{next_version}"
@@ -23,17 +23,17 @@ print(f"Successfully built image: {image_name}")
 client.images.push(repository="danielpinhas/flask-compose", tag=next_version)
 print(f"Successfully pushed image: {image_name}")
 
-# Remove the latest version image
-latest_image_name = "danielpinhas/flask-compose:latest"
-existing_latest_image = client.images.get(latest_image_name)
-if existing_latest_image:
-    client.images.remove(image=latest_image_name, force=True)
-    print(f"Successfully removed existing image: {latest_image_name}")
-
 # Tag the next version as "latest"
+latest_image_name = "danielpinhas/flask-compose:latest"
 client.images.get(image_name).tag(latest_image_name)
 print(f"Successfully tagged image as latest: {latest_image_name}")
 
 # Push the latest image to Docker Hub
 client.images.push(repository="danielpinhas/flask-compose", tag="latest")
 print(f"Successfully pushed latest image: {latest_image_name}")
+
+# Delete the previous version image
+previous_version = latest_version - 0.1
+previous_image_name = f"danielpinhas/flask-compose:{previous_version}"
+client.images.remove(image=previous_image_name, force=True)
+print(f"Successfully deleted image: {previous_image_name}")
