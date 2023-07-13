59d7661239318b22873eba5a2275b02bb90039d6
daniel <danielp1239@gmail.com>
2023-07-13 11:56:36 +0300

    K8S

--- Pipeline-K8S/Flask-Project/manageVersion.py
+++ Pipeline-K8S/Flask-Project/manageVersion.py
@@ -11,7 +11,7 @@ if existing_versions:
     next_version = latest_version + 0.1 
     # Delete the previous version image
     previous_version = latest_version
-    previous_image_name = f"danielpinhas/flask-compose:{previous_version}"
+    previous_image_name = f"danielpinhas/flask-k8s:{previous_version}"
     client.images.remove(image=previous_image_name, force=True)
     print(f"Successfully deleted image: {previous_image_name}")
 else:
@@ -19,23 +19,23 @@ else:
     
 # Format the version number to one decimal place
 next_version = f"{next_version:.1f}"
-image_name = f"danielpinhas/flask-compose:{next_version}"
+image_name = f"danielpinhas/flask-k8s:{next_version}"
 
 # Build the new version image
 client.images.build(path=".", tag=image_name, rm=True, pull=True)
 print(f"Successfully built image: {image_name}")
 
 # Push the image to Docker Hub
-client.images.push(repository="danielpinhas/flask-compose", tag=next_version)
+client.images.push(repository="danielpinhas/flask-k8s", tag=next_version)
 print(f"Successfully pushed image: {image_name}")
 
 # Tag the new version as "latest"
-latest_image_name = "danielpinhas/flask-compose:latest"
+latest_image_name = "danielpinhas/flask-k8s:latest"
 client.images.get(image_name).tag(latest_image_name)
 print(f"Successfully tagged image as latest: {latest_image_name}")
 
 # Push the latest image to Docker Hub
-client.images.push(repository="danielpinhas/flask-compose", tag="latest")
+client.images.push(repository="danielpinhas/flask-k8s", tag="latest")
 print(f"Successfully pushed latest image: {latest_image_name}")
 
 # Remove the intermediate images without a tag
