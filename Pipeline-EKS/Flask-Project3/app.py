5089ef43201404469f98b039ac25ce63641079e1
daniel <danielp1239@gmail.com>
2023-07-19 11:00:05 +0300

    M

--- Pipeline-EKS/Flask-Project3/app.py
+++ Pipeline-EKS/Flask-Project3/app.py
@@ -2,50 +2,18 @@ from flask import Flask, render_template
 import os
 import random
 import mysql.connector
-import boto3
-import json
-from botocore.exceptions import ClientError
-
-def get_secret():
-
-    secret_name = "mysql-gifs-rds"
-    region_name = "us-east-2"
-
-    # Create a Secrets Manager client
-    session = boto3.session.Session()
-    client = session.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    try:
-        get_secret_value_response = client.get_secret_value(
-            SecretId=secret_name
-        )
-    except ClientError as e:
-        # For a list of exceptions thrown, see
-        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-        raise e
-
-    # Decrypts secret using the associated KMS key.
-    secret = get_secret_value_response['SecretString']
-    return secret
 
 app = Flask(__name__)
 
 def get_random_url() -> str:
     try:
-        secret = get_secret()
-        db_credentials = json.loads(secret)
-
         config = {
-            'user': db_credentials['username'],
-            'password': db_credentials['password'],
+            'user': 'daniel2',
+            'password': 'daniel2',
             'host': 'rds-gifs-db.cih3afqd7fge.us-east-2.rds.amazonaws.com',
             'port': '3306',
             'database': 'devopsroles'
         }
-        
         connection = mysql.connector.connect(**config)
         cursor = connection.cursor()
         cursor.execute('SELECT url FROM alpacas')
