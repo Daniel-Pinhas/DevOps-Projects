10764ead0d7b86cb5fb08a9cbdd85031ffc3c4c5
daniel <danielp1239@gmail.com>
2023-07-24 15:00:10 +0300

    M

--- Pipeline-EKS/tests/prod.sh
+++ Pipeline-EKS/tests/prod.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
 # Define the Flask application URL
-FLASK_URL="http://${IPprod}:5000"
+FLASK_URL="http://${IPprod}:32000"
 sleep 10
 # Make a GET request to the Flask application
-response=$(curl -s -o /dev/null -w "%{http_code}" "http://${IPprod}:5000")
+response=$(curl -s -o /dev/null -w "%{http_code}" "http://${IPprod}:32000")
+
+# Check the HTTP response code
+if [ "$response" == "200" ]; then
+    echo "Flask application is running successfully."
+else
+    echo "Flask application is not running. HTTP response code: $response"
+    exit 1
+fi
+
+FLASK_URL="http://${IPprod}:30100"
+sleep 10
+# Make a GET request to the Flask application
+response=$(curl -s -o /dev/null -w "%{http_code}" "http://${IPprod}:30100")
+
+# Check the HTTP response code
+if [ "$response" == "200" ]; then
+    echo "Flask application is running successfully."
+else
+    echo "Flask application is not running. HTTP response code: $response"
+    exit 1
+fi
+
+FLASK_URL="http://${IPprod}:30200"
+sleep 10
+# Make a GET request to the Flask application
+response=$(curl -s -o /dev/null -w "%{http_code}" "http://${IPprod}:30200")
+
+# Check the HTTP response code
+if [ "$response" == "200" ]; then
+    echo "Flask application is running successfully."
+else
+    echo "Flask application is not running. HTTP response code: $response"
+    exit 1
+fi
+
+FLASK_URL="http://${IPprod}:30300"
+sleep 10
+# Make a GET request to the Flask application
+response=$(curl -s -o /dev/null -w "%{http_code}" "http://${IPprod}:30300")
 
 # Check the HTTP response code
 if [ "$response" == "200" ]; then
