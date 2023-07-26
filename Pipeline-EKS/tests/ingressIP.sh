ccaeb8aa88a28258027136ced7080b34269e786c
daniel <danielp1239@gmail.com>
2023-07-26 16:02:17 +0300

    M

--- Pipeline-EKS/tests/ingressIP.sh
+++ Pipeline-EKS/tests/ingressIP.sh
@@ -3,10 +3,13 @@
 # Get the Ingress IPs
 ingress_ips=($(kubectl describe service | grep Ingress | awk '{print $3}'))
 
-# Update the Jenkinsfile with the Ingress IPs
-sed -i "s/IPflask1 = '.*/IPflask1 = '${ingress_ips[0]}'/" prod.sh
-sed -i "s/IPflask2 = '.*/IPflask2 = '${ingress_ips[1]}'/" prod.sh
-sed -i "s/IPflask3 = '.*/IPflask3 = '${ingress_ips[2]}'/" prod.sh
-sed -i "s/IPprod = '.*/IPprod = '${ingress_ips[3]}'/" prod.sh
+# Define the placeholders and their corresponding Ingress IPs
+placeholders=("IPflask1" "IPflask2" "IPflask3" "IPprod")
+replace_ips=("${ingress_ips[0]}" "${ingress_ips[1]}" "${ingress_ips[2]}" "${ingress_ips[3]}")
 
-echo "Prod.sh updated successfully."
+# Iterate over the placeholders and replace them with the corresponding Ingress IPs
+for ((i = 0; i < ${#placeholders[@]}; i++)); do
+    sed -i "s/${placeholders[i]} = '.*/${placeholders[i]} = '${replace_ips[i]}'/" prod.sh
+done
+
+echo "prod.sh updated successfully."
